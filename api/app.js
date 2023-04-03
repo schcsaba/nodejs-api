055c998c5262027fa9e1a853f7a5cf9af363fde0
Csaba Schnitchen <schcsaba@gmail.com>
2023-04-03 08:43:03 +0200

    feat: add validation, database connection

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@nodejs01-mongo.eg2g7ue.mongodb.net/messages?retryWrites=true&w=majority`;
 
 const app = express();
 
@@ -16,4 +19,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+    .connect(MONGODB_URI)
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,14 +1,32 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{ title: 'First Post', content: 'This is the first post!' }]
+        posts: [
+            {
+                _id: '1',
+                title: 'First Post',
+                content: 'This is the first post!',
+                imageUrl: 'images/Mock-Up_BlueBookCopernicus_2.jpg',
+                creator: { name: 'Csaba' },
+                createdAt: new Date()
+            }
+        ]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed, entered data is incorrect.',
+            errors: errors.array()
+        });
+    }
     const title = req.body.title;
     const content = req.body.content;
     res.status(201).json({
         message: 'Post created successfully!',
-        post: { id: new Date().toISOString, title: title, content: content }
+        post: { _id: new Date().toISOString, title: title, content: content, creator: { name: 'Csaba' }, createdAt: new Date() }
     });
 };
