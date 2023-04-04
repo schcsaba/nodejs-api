519926cee857c60fcbd69c775c98553fc042da7a
Csaba Schnitchen <schcsaba@gmail.com>
2023-04-04 08:57:26 +0200

    feat: add error handling, serve posts with images

--- api/app.js
+++ api/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,6 +11,7 @@ const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,6 +21,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
 
 mongoose
     .connect(MONGODB_URI)
--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -3,27 +3,24 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: '1',
-                title: 'First Post',
-                content: 'This is the first post!',
-                imageUrl: 'images/Mock-Up_BlueBookCopernicus_2.jpg',
-                creator: { name: 'Csaba' },
-                createdAt: new Date()
+    Post.find()
+        .then(posts => {
+            res.status(200).json({ message: 'Fetched posts successfully.', posts: posts })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
             }
-        ]
-    });
+            next(err);
+        });
 };
 
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: 'Validation failed, entered data is incorrect.',
-            errors: errors.array()
-        });
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
     }
     const title = req.body.title;
     const content = req.body.content;
@@ -41,6 +38,28 @@ exports.createPost = (req, res, next) => {
             });
         })
         .catch(err => {
-            console.log(err);
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({ message: 'Post fetched.', post: post })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
 };
