bbe3d6c4a695796ea28fe55c8b3a6b1fb86291b7
Csaba Schnitchen <schcsaba@gmail.com>
2023-04-06 07:53:37 +0200

    feat: add image upload, post update and delete, pagination

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,11 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    Post.find()
-        .then(posts => {
-            res.status(200).json({ message: 'Fetched posts successfully.', posts: posts })
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
+    Post.find().countDocuments()
+        .then(count => {
+            totalItems = count;
+            return Post.find().skip((currentPage - 1) * perPage).limit(perPage);
+        }).then(posts => {
+            res.status(200).json({ message: 'Fetched posts successfully.', posts: posts, totalItems: totalItems })
         })
         .catch(err => {
             if (!err.statusCode) {
@@ -22,12 +31,18 @@ exports.createPost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
+    if (!req.file) {
+        const error = new Error('No image provided.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
     const post = new Post({
         title: title,
         content: content,
-        imageUrl: 'images/Mock-Up_BlueBookCopernicus_2.jpg',
+        imageUrl: imageUrl,
         creator: { name: 'Csaba' }
     });
     post.save()
@@ -62,4 +77,79 @@ exports.getPost = (req, res, next) => {
             }
             next(err);
         });
+};
+
+exports.updatePost = (req, res, next) => {
+    const postId = req.params.postId;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if (req.file) {
+        imageUrl = req.file.path;
+    }
+    if (!imageUrl) {
+        const error = new Error('No image picked.');
+        error.statusCode = 422;
+        throw error;
+    }
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            if (imageUrl !== post.imageUrl) {
+                clearImage(post.imageUrl);
+            }
+            post.title = title;
+            post.imageUrl = imageUrl;
+            post.content = content;
+            return post.save();
+        })
+        .then(result => {
+            res.status(200).json({ message: 'Post updated!', post: result });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+exports.deletePost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            // check logged in user
+            clearImage(post.imageUrl);
+            return Post.findByIdAndRemove(postId);
+        })
+        .then(result => {
+            console.log(result);
+            res.status(200).json({ message: 'Deleted post.' });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
 };
